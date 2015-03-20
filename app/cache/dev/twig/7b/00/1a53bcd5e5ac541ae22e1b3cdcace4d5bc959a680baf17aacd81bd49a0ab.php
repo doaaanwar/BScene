e09c1e0fa6f18149799e4b81bf32d39df3e974d7
Mahmoud Jallala <mjallala@gmail.com>
2015-03-20 14:44:26 -0400

    Speaker and EventComments

---
+++ app/cache/dev/twig/7b/00/1a53bcd5e5ac541ae22e1b3cdcace4d5bc959a680baf17aacd81bd49a0ab.php
@@ -0,0 +1,327 @@
+<?php
+
+/* base.html.twig */
+class __TwigTemplate_7b001a53bcd5e5ac541ae22e1b3cdcace4d5bc959a680baf17aacd81bd49a0ab extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'nav' => array($this, 'block_nav'),
+            'javascripts' => array($this, 'block_javascripts'),
+            'header' => array($this, 'block_header'),
+            'body' => array($this, 'block_body'),
+            'footer' => array($this, 'block_footer'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\" />
+        <title>";
+        // line 5
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+       
+            <link href=\"";
+        // line 7
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmebscene/css/bootstrap.min.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\">
+            <!-- Custom CSS -->
+            <link href=\"";
+        // line 9
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmebscene/css/bscene.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\">
+        ";
+        // line 10
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 12
+        echo "        <link rel=\"icon\" type=\"image/x-icon\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("favicon.ico"), "html", null, true);
+        echo "\" />
+    </head>
+    <body>
+        
+            <!-- Navigation -->
+            <nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">
+                <div class=\"container\">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class=\"navbar-header\">
+                        <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">
+                            <span class=\"sr-only\">Toggle navigation</span>
+                            <span class=\"icon-bar\"></span>
+                            <span class=\"icon-bar\"></span>
+                            <span class=\"icon-bar\"></span>
+                        </button>
+                        <a class=\"navbar-brand\" href=\"#\"> B-Scene</a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+                        <ul class=\"nav navbar-nav\">
+                            <li>
+                                <a href=\"#\">About</a>
+                            </li>
+                            <li>
+                                <a href=\"#\">Services</a>
+                            </li>
+                            <li>
+                                <a href=\"#\">Contact</a>
+                            </li>
+                        </ul>
+                        <form class=\"navbar-form navbar-left\" role=\"search\">
+                            <div class=\"form-group\">
+                                <input class=\"form-control\" type=\"text\" />
+                            </div> 
+                            <button type=\"button\" class=\"btn btn-default\" onclick=\"'#'\">Search</button>
+                        </form>
+                        <ul class=\"nav navbar-nav navbar-right\">
+                            ";
+        // line 49
+        if (($this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "session", array()), "get", array(0 => "admin"), "method") != null)) {
+            // line 50
+            echo "                                <li>
+                                           <a href=\"admin.html\">Dashboard</a>
+                                </li>
+                            ";
+        }
+        // line 53
+        echo " 
+                            ";
+        // line 54
+        if (($this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "session", array()), "get", array(0 => "member"), "method") != null)) {
+            // line 55
+            echo "                                
+                                 <li class=\"dropdown\">
+                                           <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"fa fa-user\"></i> Welcome ";
+            // line 57
+            echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "session", array()), "get", array(0 => "member"), "method"), "html", null, true);
+            echo " <b class=\"caret\"></b></a>
+                                           <ul class=\"dropdown-menu\">
+                                               <li>
+                                                   <a href=\"profile.html\"><i class=\"fa fa-fw fa-user\"></i> Profile</a>
+                                               </li>
+
+                                               <li>
+                                                   <a href=\"createEvent.html\"><i class=\"fa fa-fw fa-gear\"></i> Post Event</a>
+                                               </li>
+                                               <li class=\"divider\"></li>
+                                               <li>
+                                                   <a href=\"index.html\"><i class=\"fa fa-fw fa-power-off\"></i> Log Out</a>
+                                               </li>
+                                          </ul>
+                                  </li>
+                            ";
+        } else {
+            // line 73
+            echo "                            
+                                    <li>
+                                        <a class=\"login-window\" href=\"#login-box\">Login</a>
+                                    </li>
+                                    <li>
+                                        <a href=\"#\">Sign up</a>
+                                    </li>
+                            ";
+        }
+        // line 81
+        echo "                        </ul>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                    ";
+        // line 84
+        $this->displayBlock('nav', $context, $blocks);
+        // line 86
+        echo "                </div>
+                <!-- /.container -->
+            </nav>
+            
+            
+                <script src=\"";
+        // line 91
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmebscene/js/jquery.js"), "html", null, true);
+        echo "\"></script>
+                <!-- Bootstrap Core JavaScript -->
+                <script src=\"";
+        // line 93
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmebscene/js/bootstrap.min.js"), "html", null, true);
+        echo "\"></script>
+                ";
+        // line 94
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 99
+        echo "                <div id=\"login-box\" class=\"login-popup\">
+                    <script type=\"text/javascript\">
+                                \$(document).ready(function () {
+                                    \$('a.login-window').click(function () {
+
+                                        //Getting the variable's value from a link 
+                                        var loginBox = \$(this).attr('href');
+
+                                        //Fade in the Popup
+                                        \$(loginBox).fadeIn(300);
+
+                                        //Set the center alignment padding + border see css style
+                                        var popMargTop = (\$(loginBox).height() + 24) / 2;
+                                        var popMargLeft = (\$(loginBox).width() + 24) / 2;
+
+                                        \$(loginBox).css({
+                                            'margin-top': -popMargTop,
+                                            'margin-left': -popMargLeft
+                                        });
+
+                                        // Add the mask to body
+                                        \$('body').append('<div id=\"mask\"></div>');
+                                        \$('#mask').fadeIn(300);
+
+                                        return false;
+                                    });
+
+                                    // When clicking on the button close or the mask layer the popup closed
+                                    \$('a.close, #mask').on('click', function () {
+                                        \$('#mask , .login-popup').fadeOut(300, function () {
+                                            \$('#mask').remove();
+                                        });
+                                        return false;
+                                    });
+                                });
+                    </script>
+                
+                <a href=\"#\" class=\"close\"><img src=\"";
+        // line 136
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmebscene/images/close_pop.png"), "html", null, true);
+        echo "\" class=\"btn_close\" title=\"Close Window\" alt=\"Close\" /></a>
+                <form  class=\"signin\" method=\"POST\" action=\"";
+        // line 137
+        echo $this->env->getExtension('routing')->getPath("acmebscene_login");
+        echo "\">
+                    
+                    
+                    <fieldset class=\"textbox\">
+                        <label class=\"username\">
+                            <span>Username or email</span>
+                            <input id=\"username\" name=\"username\" value=\"\" type=\"text\" autocomplete=\"on\" placeholder=\"Username\">
+                        </label>
+                        <label class=\"password\">
+                            <span>Password</span>
+                            <input id=\"password\" name=\"password\" value=\"\" type=\"password\" placeholder=\"Password\">
+                        </label>
+                        <button class=\"button\" type=\"submit\">Sign in</button>
+                        <p>
+                            <a class=\"forgot\" href=\"forgetPassword.html\">Forgot your password?</a>
+                        </p>        
+                    </fieldset>
+                </form>
+            </div>
+             <!-- Page Content -->
+            <div class=\"container\">
+                ";
+        // line 158
+        $this->displayBlock('header', $context, $blocks);
+        // line 161
+        echo "                 
+
+                <div>
+                     ";
+        // line 164
+        $this->displayBlock('body', $context, $blocks);
+        // line 167
+        echo "                </div>
+                
+                <!-- Footer -->
+                <footer>
+                    <div class=\"row\">
+                        <div class=\"col-lg-12\">
+                            <p>Copyright &copy; B-Scene 2015</p>
+                        </div>
+                    </div>
+                    ";
+        // line 176
+        $this->displayBlock('footer', $context, $blocks);
+        // line 179
+        echo "                </footer>
+
+            </div>
+            
+        
+    </body>
+</html>
+";
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = array())
+    {
+        echo "B-Scene";
+    }
+
+    // line 10
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        echo "\t
+        ";
+    }
+
+    // line 84
+    public function block_nav($context, array $blocks = array())
+    {
+        // line 85
+        echo "                    ";
+    }
+
+    // line 94
+    public function block_javascripts($context, array $blocks = array())
+    {
+        // line 95
+        echo "                    
+                    
+                    
+                ";
+    }
+
+    // line 158
+    public function block_header($context, array $blocks = array())
+    {
+        // line 159
+        echo "                       
+                 ";
+    }
+
+    // line 164
+    public function block_body($context, array $blocks = array())
+    {
+        // line 165
+        echo "                       
+                     ";
+    }
+
+    // line 176
+    public function block_footer($context, array $blocks = array())
+    {
+        // line 177
+        echo "                       
+                    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "base.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  309 => 177,  306 => 176,  301 => 165,  298 => 164,  293 => 159,  290 => 158,  283 => 95,  280 => 94,  276 => 85,  273 => 84,  266 => 10,  260 => 5,  249 => 179,  247 => 176,  236 => 167,  234 => 164,  229 => 161,  227 => 158,  203 => 137,  199 => 136,  160 => 99,  158 => 94,  154 => 93,  149 => 91,  142 => 86,  140 => 84,  135 => 81,  125 => 73,  106 => 57,  102 => 55,  100 => 54,  97 => 53,  91 => 50,  89 => 49,  48 => 12,  46 => 10,  42 => 9,  37 => 7,  32 => 5,  26 => 1,);
+    }
+}
---
+++ app/cache/dev/twig/fb/81/85b1b6bed2a92dbbd13b551c06140df1aaa0c56fb1220a26fba28f48a99f.php
@@ -0,0 +1,197 @@
+<?php
+
+/* AcmebsceneBundle:Account:show.html.twig */
+class __TwigTemplate_fb8185b1b6bed2a92dbbd13b551c06140df1aaa0c56fb1220a26fba28f48a99f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("::base.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "<h1>Account</h1>
+
+    <table class=\"record_properties\">
+        <tbody>
+            <tr>
+                <th>Id</th>
+                <td>";
+        // line 10
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Username</th>
+                <td>";
+        // line 14
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "username", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Password</th>
+                <td>";
+        // line 18
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "password", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Firstname</th>
+                <td>";
+        // line 22
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "firstName", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Lastname</th>
+                <td>";
+        // line 26
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "lastName", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Email</th>
+                <td>";
+        // line 30
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "email", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Backupemail</th>
+                <td>";
+        // line 34
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "backupEmail", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Businessphone</th>
+                <td>";
+        // line 38
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "businessPhone", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Url</th>
+                <td>";
+        // line 42
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "url", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Address1</th>
+                <td>";
+        // line 46
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "address1", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Address2</th>
+                <td>";
+        // line 50
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "address2", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Province</th>
+                <td>";
+        // line 54
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "province", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Paymentamount</th>
+                <td>";
+        // line 58
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "paymentAmount", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Membersince</th>
+                <td>";
+        // line 62
+        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "memberSince", array()), "Y-m-d H:i:s"), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Isadmin</th>
+                <td>";
+        // line 66
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "isAdmin", array()), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Lastlogin</th>
+                <td>";
+        // line 70
+        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "lastLogin", array()), "Y-m-d H:i:s"), "html", null, true);
+        echo "</td>
+            </tr>
+        </tbody>
+    </table>
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 77
+        echo $this->env->getExtension('routing')->getPath("account");
+        echo "\">
+            Back to the list
+        </a>
+    </li>
+    <li>
+        <a href=\"";
+        // line 82
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("account_edit", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id", array()))), "html", null, true);
+        echo "\">
+            Edit
+        </a>
+    </li>
+    <li>";
+        // line 86
+        echo         $this->env->getExtension('form')->renderer->renderBlock((isset($context["delete_form"]) ? $context["delete_form"] : $this->getContext($context, "delete_form")), 'form');
+        echo "</li>
+</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmebsceneBundle:Account:show.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  177 => 86,  170 => 82,  162 => 77,  152 => 70,  145 => 66,  138 => 62,  131 => 58,  124 => 54,  117 => 50,  110 => 46,  103 => 42,  96 => 38,  89 => 34,  82 => 30,  75 => 26,  68 => 22,  61 => 18,  54 => 14,  47 => 10,  39 => 4,  36 => 3,  11 => 1,);
+    }
+}
