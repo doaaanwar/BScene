0eb47f527e6b1fd19f9ccf3e86b01a9cfd9fcaaf
vbetts <toribetts@gmail.com>
2015-03-22 20:11:11 -0400

    fixed required fields in new account

--- src/Acme/bsceneBundle/Form/AccountType.php
+++ src/Acme/bsceneBundle/Form/AccountType.php
@@ -17,7 +17,7 @@ class AccountType extends AbstractType
         $builder
             ->add('username', 'text', array(
                 'label' => '* Username',
-                'required' => 'true',
+                'required' => true,
                 'attr' => array('name' =>'username')))
             
             ->add('password', 'repeated', array(
@@ -26,90 +26,90 @@ class AccountType extends AbstractType
                 'first_options'  => array(
                     'label' => '* Password'),
                 'second_options' => array('label' => '* Repeat Password'),
-                'required' => 'true',
+                'required' => true,
                 'type' => 'password',
                 'attr' => array ('name' => 'password')))
             
             ->add('firstName', 'text', array (
                 'label' => '* First Name',
-                'required' => 'true',
+                'required' => true,
                 'attr' => array('name' => 'firstName')))
                 
             ->add('lastName', 'text', array (
                 'label' => '* Last Name',
-                'required' => 'true',
+                'required' => true,
                 'attr' => array('name' => 'lastName')))
                 
             ->add('email', 'email', array (
                 'label' => '* E-mail Address',
-                'required' => 'true',
+                'required' => true,
                 'attr' => array ('name' => 'email')))
                 
             ->add('backupEmail', 'hidden', array (
                 'label' => 'Secondary E-mail Address',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'backupEmail')))
                 
             ->add('businessPhone', 'text', array (
                 'label' => '* Business Phone',
-                'required' => 'true',
+                'required' => true,
                 'attr' => array('name' => 'busPhone')))
                 
             ->add('url', 'url', array (
                 'label' => 'URL',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'memberUrl')))
                 
             ->add('address1', 'text', array (
                 'label' => 'Address Line 1',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'address1')))
                 
             ->add('address2', 'text', array (
                 'label' => 'Address Line 2',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'address2')))
                 
             ->add('province', 'entity', array (
                 'class' => 'AcmebsceneBundle:Province',
                 'label' => 'Province',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'prov')))
                 
             ->add('paymentAmount', 'hidden', array (
                 'label' => 'Payment Amount',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'fee')))
                 
             ->add('memberSince', 'hidden', array (
                 'label' => 'Member Since: ',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'memberSince')))
                 
             ->add('isAdmin', 'hidden', array (
                 'label' => 'Admin',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'isAdmin')))
                 
             ->add('lastLogin','hidden',  array (
                 'label' => 'Last Login: ',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'lastLogin')))
                 
             ->add('city', 'text', array (
                 'label' => 'City',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'city')))
                 
             ->add('organization', 'entity', array (
                 'class' => 'AcmebsceneBundle:Organization',
                 'label' => '* Organization/Company',
-                'required' => 'true',
+                'required' => true,
                 'attr' => array('name' => 'organization')))
                 
             ->add('status', 'hidden', array (
                 'label' => 'Account Status',
-                'required' => 'false',
+                'required' => false,
                 'attr' => array('name' => 'accountStatus')))
                             
         ;
