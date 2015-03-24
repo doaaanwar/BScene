e92ba08662d791f71069d57b9fb38863026160b0
doaa <doaa@localhost>
2015-03-24 09:16:40 -0400

    add jquery UI date picker

--- src/Acme/bsceneBundle/Form/MeetingType.php
+++ src/Acme/bsceneBundle/Form/MeetingType.php
@@ -15,19 +15,33 @@ class MeetingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('date')
+            ->add('title', 'text', array(
+                'label' => '* Event Title',
+                'required' => true,
+                'attr' => array('id' =>'title')))
+           
+            ->add('date','text', array(
+                'label' => '* Date',
+                'required' => true,
+                'attr' => array('id' =>'date')))
             ->add('time')
-            ->add('endDate')
+            ->add('endDate','text', array(
+                'label' => '* End Date',        
+                'required' => true,
+                'attr' => array('id' =>'endDate')))
             ->add('endTime')
-            ->add('description')
+            ->add('description','textarea')
             ->add('capacity')
             ->add('venue')
             ->add('organization')
-            ->add('image')
+            ->add('image','file', array(
+                'label' => "Event Logo",
+                'required' => false,
+                'attr' => array('id' => 'image')))
+           
             ->add('account')
             ->add('category')
-            ->add('speakers')
+          
         ;
     }
     
