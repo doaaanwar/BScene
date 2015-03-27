289920edaac8edbb9e3d874df2a25f95401fe5f0
vbetts <toribetts@gmail.com>
2015-03-27 12:11:26 -0400

    small changes to templates, added one event

--- src/Acme/bsceneBundle/Form/MeetingType.php
+++ src/Acme/bsceneBundle/Form/MeetingType.php
@@ -6,55 +6,48 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class MeetingType extends AbstractType
-{
+class MeetingType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('title', 'text', array(
-                'label' => '* Event Title',
-                'required' => true,
-               
-                'attr' => array('id' =>'title')))
-           
-            ->add('date','text', array(
-                'label' => '* Date',
-                
-                'required' => true,
-                'attr' => array('id' =>'date')))
-            ->add('time')
-            ->add('endDate','text', array(
-                'label' => 'End Date',
-                 
-                'attr' => array('id' =>'endDate')))
-            ->add('endTime')
-            ->add('description','textarea')
-            ->add('capacity')
-            ->add('venue','hidden')
-            ->add('organization','hidden', array(
-                'data_class' => 'Acme\bsceneBundle\Entity\Organization'
-            ))
-            ->add('image','file',array(
-                'data_class' => 'Acme\bsceneBundle\Entity\Image'
-            ))
-           
-            ->add('account','hidden', array(
-                'data_class' => 'Acme\bsceneBundle\Entity\Account'
-            ))
-            ->add('category')
-          
+                ->add('title', 'text', array(
+                    'label' => '* Event Title',
+                    'required' => true,
+                    'attr' => array('id' => 'title')))
+                ->add('date', 'text', array(
+                    'label' => '* Date',
+                    'required' => true,
+                    'attr' => array('id' => 'date')))
+                ->add('time')
+                ->add('endDate', 'text', array(
+                    'label' => 'End Date',
+                    'attr' => array('id' => 'endDate')))
+                ->add('endTime')
+                ->add('description', 'textarea')
+                ->add('capacity')
+                ->add('venue', 'hidden')
+                ->add('organization', 'hidden', array(
+                    'data_class' => 'Acme\bsceneBundle\Entity\Organization'
+                ))
+                ->add('image', 'file', array(
+                    'data_class' => 'Acme\bsceneBundle\Entity\Image'
+                ))
+                ->add('account', 'hidden', array(
+                    'data_class' => 'Acme\bsceneBundle\Entity\Account'
+                ))
+                ->add('category')
+
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Acme\bsceneBundle\Entity\Meeting'
         ));
@@ -63,8 +56,8 @@ class MeetingType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'acme_bscenebundle_meeting';
     }
+
 }
