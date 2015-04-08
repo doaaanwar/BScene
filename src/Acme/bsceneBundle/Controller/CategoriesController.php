72b4aa1b2b4ecfa0e5a56f14d6c2b1081d188754
doaa <doaa@localhost>
2015-04-08 04:44:04 -0400

    save uploaded image on category update

--- src/Acme/bsceneBundle/Controller/CategoriesController.php
+++ src/Acme/bsceneBundle/Controller/CategoriesController.php
@@ -192,36 +192,21 @@ class CategoriesController extends Controller
          
         if ($editForm->isValid()) {
             $imageEntity = NULL;
-            //commented till finish implementation
-            if (($image instanceof UploadedFile) && ($image->getError() == '0')) {
-                $originalName = $image->getClientOriginalName();
-                $name_array = explode('.', $originalName);
-                $file_type = $name_array[sizeof($name_array) - 1];
-                $valid_filetypes = array('jpg', 'jpeg', 'png', 'bmp');
-                if (in_array(strtolower($file_type), $valid_filetypes)) {
-                //upload and save the path to the image.url
-                    $imageEntity = new Image();
-                    $imageEntity->setFile($image);
-                //TODO check if name already there
-                    $imageEntity->setName($originalName);
-                    $imageEntity->upload();
-                //TODO set the URL/path
-                    $imageEntity->setURL($imageEntity->getWebPath());
-                    $em = $this->getDoctrine()->getManager();
-                    $em->persist($imageEntity);
-                    $em->flush();
+        
+            if ($image) {
+                if (($image instanceof UploadedFile) && ($image->getError() == '0')) {
+                    //call upload image
+                    $imageEntity = $this->uploadImage($image);
                     $entity->setImage($imageEntity);
                 } else {
-                    print_r("Invalid file type");
-                    //die();
+                    print_r($image->getError());
+                    die();
                 }
-            } else {
-                print_r("image upload error");
-                //die();
             }
+
             $em->flush();
 
-            return $this->redirect($this->generateUrl('category_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('category'));
         }
 
         return $this->render('AcmebsceneBundle:Categories:edit.html.twig', array(
