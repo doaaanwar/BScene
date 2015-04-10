149d8e7c4e7100943835cb8d8cc8762055d37f4f
doaa <doaa@localhost>
2015-04-10 04:34:49 -0400

    upload image error message on category controller

--- src/Acme/bsceneBundle/Controller/CategoriesController.php
+++ src/Acme/bsceneBundle/Controller/CategoriesController.php
@@ -62,9 +62,16 @@ class CategoriesController extends Controller
             if (($image instanceof UploadedFile) && ($image->getError() == '0')) {
                 //call upload image
                 $imageEntity = $this->uploadImage($image);
-                $entity->setImage($imageEntity);
+                if($imageEntity)
+                {
+                    $entity->setImage($imageEntity);
+                }
+                else {
+                     $form->addError(new FormError("The image selected not a invalid type of image."));
+                }
+                
             } else {
-                print_r($image->getError());
+               $form->addError(new FormError("Error in uploading the image."));
             }
         } else {
             //add mantatory error
@@ -215,13 +222,19 @@ class CategoriesController extends Controller
             if (($image instanceof UploadedFile) && ($image->getError() == '0')) {
                 //call upload image
                 $imageEntity = $this->uploadImage($image);
-                $entity->setImage($imageEntity);
+                if($imageEntity)
+                {
+                    $entity->setImage($imageEntity);
+                }
+                else {
+                   $editForm->addError(new FormError("The image selected not a invalid type of image."));
+                }
             } else {
-                print_r($image->getError());
+                 $form->addError(new FormError("Error in uploading the image."));
             }
         }
         else {
-              $entity->setImage($imageOld);
+              $editForm->setImage($imageOld);
         }
          
         if ($editForm->isValid()) {
@@ -264,8 +277,7 @@ class CategoriesController extends Controller
             $em->flush();
             return $imageEntity;
         } else {
-            print_r("Invalid file type");
-            die();
+            return null;
         }
     }
 
